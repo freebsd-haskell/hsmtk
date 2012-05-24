627accba3dfc7216e43282f6c16715af6c1d13b8
Gabor Pali <pali.gabor@gmail.com>
2012-05-24 11:50:30 +0200

    hs-depcalc: Improve parameter lists and add a new function for checking what ports are not yet currently integrated

--- hs-depcalc.hs
+++ hs-depcalc.hs
@@ -1,12 +1,17 @@
 
 import Control.Applicative((<$>))
+import Control.Monad
 import Data.Char(isSpace)
 import Data.ByteString.Lazy(readFile)
 import Data.ByteString.Lazy.Char8(concat,singleton,split,init,last,unpack,lines)
 import Data.Graph
-import Data.List(intersperse,sort)
+import Data.List(intersperse,sort,nub)
 import System.Environment
 import Prelude hiding (readFile,concat,init,last,lines)
+import qualified Prelude as P
+import System.FilePath.Posix
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 x |> f = f x
 
@@ -43,14 +48,16 @@ printLines = putStrLn . unlines
 -- The result is a list of all dependants according to the direction of
 -- search: "forward" or "backward" (inverted).
 --
-calculateDependency describe port dir = do
+calculateDependency describe dir port = do
   contents <- readFile describe
   let (h,f,g) = contents |> portify |> generateGraph
   return $ depends (dir h) port f g translate
 
-showDependency describe port dir = do
-  res <- calculateDependency describe port dir
-  printLines res
+showDependency describe dir port =
+  calculateDependency describe dir port >>= printLines
+
+showDependencyL describe dir = do
+  liftM (nub . sort . P.concat) . mapM (calculateDependency describe dir)
 
 --
 -- Calculate a topology for ports -- it is an ordering between graph
@@ -59,12 +66,22 @@ showDependency describe port dir = do
 --
 -- The result is a list ordered by their dependencies.
 --
-calculateTopology describe port dir = do
+calculateTopology describe dir port = do
   contents <- readFile describe
   let (h,f,g) = contents |> portify |> generateGraph
   let (h1,f1,g1) = graphFromEdges $ depends (dir h) port f g id
   return $ topSort h1 |> map (translate . f1) |> reverse
 
-showTopology describe port dir = do
-  res <- calculateTopology describe port dir
-  printLines res
+showTopology describe dir port =
+  calculateTopology describe dir port >>= printLines
+
+hackageMk = "lang/ghc/bsd.hackage.mk"
+
+inTree portsdir = do
+  let pid = T.pack "_port="
+  contents <- fmap (P.map T.strip . T.lines) $ T.readFile (portsdir </> hackageMk)
+  return $ P.map (T.unpack . P.fst . T.breakOn pid) $ P.filter (pid `T.isInfixOf`) contents
+
+notInTree portsdir ports = do
+  intree <- inTree portsdir
+  return $ filter (`notElem` intree) ports
