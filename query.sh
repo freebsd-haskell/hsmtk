c764ea100610083ab24ff6ef43a0ca6b7f996e82
Gabor Pali <pali.gabor@gmail.com>
2015-08-19 19:55:52 +0200

    query: Remove collecting ports, read the list of ports to query from stdin

--- query.sh
+++ query.sh
@@ -9,10 +9,7 @@ else
   VAR=$1
 fi
 
-hackagedb="${PORTSDIR}/lang/ghc/bsd.hackage.mk"
-hs_ports=$(cat $hackagedb | grep _port= | tr -d "\t " | sed 's/\([^=]*\)=\([^#]*\)\(.*\)/\2/' | sort)
-
-for hs_port in $hs_ports; do
+while read hs_port; do
   if [ -d ${PORTSDIR}/$hs_port ]; then
     var=$(make -C ${PORTSDIR}/$hs_port -V ${VAR})
     echo $hs_port $var
